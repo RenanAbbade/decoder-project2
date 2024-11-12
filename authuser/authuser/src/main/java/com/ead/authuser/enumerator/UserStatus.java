12ae3e7c581a3ec2f10d053f0c90d51b19cb0587
RenanAbbade <renanabbade1997@gmail.com>
2024-11-11 23:55:51 -0300

    JPA & Hibernate modelagem

---
+++ authuser/authuser/src/main/java/com/ead/authuser/enumerator/UserStatus.java
@@ -0,0 +1,6 @@
+package com.ead.authuser.enumerator;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED
+}
---
+++ authuser/authuser/src/main/java/com/ead/authuser/enumerator/UserType.java
@@ -0,0 +1,8 @@
+package com.ead.authuser.enumerator;
+
+public enum UserType {
+    ADMIN,
+    USER,
+    STUDENT,
+    INSTRUCTOR
+}
